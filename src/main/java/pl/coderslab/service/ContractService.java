dd7957e78984ad5373d3a70e7d7c01d447da6637
Michal Biedzki <mbiedzki@me.com>
2019-03-15 16:19:03 +0100

    2019.03.15

--- src/main/java/pl/coderslab/service/ContractService.java
+++ src/main/java/pl/coderslab/service/ContractService.java
@@ -1,9 +1,5 @@
 package pl.coderslab.service;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
